464ee8f1383f3ba6362212d23f5b3d5d9b42adf0
Dejan Ljutic <dejan.ljutic@devcenter.rs>
2019-10-23 11:42:51 +0200

    init commit

---
+++ src/emails/account.js
@@ -0,0 +1,27 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        // html: property za ubacivanje slika i fensi stvari
+        to: email,
+        from: 'dejan.ljutic@devcenter.rs',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'dejan.ljutic@devcenter.rs',
+        subject: 'Goodbye',
+        text: `We are sad to see you go, ${name}. We would appreciate it if you spared a minute to tell us how we could improve!`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
