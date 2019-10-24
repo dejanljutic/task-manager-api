71147dbfbfff4a83f6ba0c6e3a19f36026ac1667
Dejan Ljutic <dejan.ljutic@devcenter.rs>
2019-10-24 13:18:58 +0200

    added tests

--- src/routers/task.js
+++ src/routers/task.js
@@ -60,7 +60,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
 
         res.send(task)
     } catch (e) {
-        res.status(500).send(e)
+        res.status(500).send()
     }
 })
 
@@ -83,7 +83,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
         res.send(task)
     } catch (e) {
-        res.send(400).send(e)
+        res.status(400).send(e)
     }
 })
 
@@ -92,7 +92,7 @@ router.delete('/tasks/:id', auth, async (req, res) => {
         const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
         if (!task) 
-            return res.status(404).send()
+            res.status(404).send()
 
         res.send(task)
     } catch (e) {
